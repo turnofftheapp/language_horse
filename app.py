46c4e77a5c1d619db151a86844d05eac5e119ef0
Max Carey <maxcarey@protonmail.com>
2020-10-30 10:52:30 -0600

    Add internal routing logic

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-# Let's render the first route
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
+
+
+@app.route('/input')
+def view_abc():
+    return render_template('input.html')
+
+
+@app.route('/win')
+def win_or_loose():
+    return render_template('output-wrong.html')

c47f3151f18b1f77e23cac2876f090a870e7664d
Max Carey <maxcarey@protonmail.com>
2020-11-09 14:46:24 -0600

    Read in pickle and pass it in to input.html

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, url_for
 import random
+import pickle
+
+# Import the pickle file which contains the langauge pairs
+infile = open('save.p','rb')
+langs = pickle.load(infile)
+infile.close()
 
 app = Flask(__name__)
 
@@ -10,7 +16,7 @@ def home():
 
 @app.route('/input')
 def view_abc():
-    return render_template('input.html')
+    return render_template('input.html', langs=langs)
 
 
 @app.route('/win')

a59dd512beb861195fc297909f746c040d4e5bc2
Max Carey <maxcarey@protonmail.com>
2020-11-09 18:23:15 -0600

    Plug in app.js file in right place and add a test button

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+// When you select languages, and click the next button
+$( "#translate_next" ).click(function() {
+  console.log( "Handler for .click() called." );
+});
+
+
+
+//var option = $('option:selected', this).attr('mytag');
+//console.log(option);

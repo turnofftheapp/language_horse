005cd9332c79f63b80deae938b811e5572c5daa5
Max Carey <maxcarey@protonmail.com>
2020-11-11 11:02:45 -0600

    Grab the langauge data from the UI

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,17 @@
+// Initialize global variables that will hold the key langauges
+var translateFromLang = "";
+var translateToLang = ""
+var translateFromLangCode = "";
+var translateToLangCode = "";
+
 // When you select languages, and click the next button
 $( "#translate_next" ).click(function() {
-  console.log( "Handler for .click() called." );
+  // Grab the langauge data from the UI
+  translateFromLang = $("#ddl_lang_from :selected").text()
+  translateToLang = $("#ddl_lang_to :selected").text()
+  translateFromLangCode = $("#ddl_lang_from :selected").attr('label')
+  translateToLangCode = $("#ddl_lang_to :selected").attr('label')
+
 });
 
 
-
-//var option = $('option:selected', this).attr('mytag');
-//console.log(option);

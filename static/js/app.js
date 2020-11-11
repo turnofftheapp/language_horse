528d65867dafabcf362be289a792b7857552ab0a
Max Carey <maxcarey@protonmail.com>
2020-11-11 12:23:44 -0600

    Populate text fields on last input string with test data

--- static/js/app.js
+++ static/js/app.js
@@ -3,14 +3,15 @@ var translateFromLang = "";
 var translateToLang = ""
 var translateFromLangCode = "";
 var translateToLangCode = "";
+var targetL1Word = "";
 
 // When you select languages, and click the next button
 $( "#translate_next" ).click(function() {
   // Grab the langauge data from the UI
-  translateFromLang = $("#ddl_lang_from :selected").text()
-  translateToLang = $("#ddl_lang_to :selected").text()
-  translateFromLangCode = $("#ddl_lang_from :selected").attr('label')
-  translateToLangCode = $("#ddl_lang_to :selected").attr('label')
+  translateFromLang = $("#ddl_lang_from :selected").text();
+  translateToLang = $("#ddl_lang_to :selected").text();
+  translateFromLangCode = $("#ddl_lang_from :selected").attr('label');
+  translateToLangCode = $("#ddl_lang_to :selected").attr('label');
 
   // Change the text of the badges based on previous codes
   $('#translate-from-label').text(translateFromLang);
@@ -18,12 +19,25 @@ $( "#translate_next" ).click(function() {
 
 });
 
-// When you hit the translate button we should make an AJAX
+// When you hit the translate button we should make an AJAX 
 $( "#translate-button" ).click(function() {
+  
+  // Get the word from the text box
+  targetL1Word = $('#L1-input-text-box').val();
+
   // Placeholder code for button, we will put ajax call here
-  $.ajax({url: "/translate", success: function(result) {
+  var translateURL = "/translate" + "/" + translateFromLangCode + "/" + translateToLangCode + "/" + targetL1Word;
+
+
+  $.ajax({url: translateURL, success: function(result) {
+    // Print out the resulting data for testing purposes, remove later
     console.log(result);
+    
+    // Update the translate_to box with the result
     $("#L2-target-word").text(result);
+    
+    // Update the transalated_from box with the target L1 word
+    $("#L1-target-word").text(targetL1Word);
   }});
 });
 

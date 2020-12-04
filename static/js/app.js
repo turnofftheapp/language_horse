084612e98a7f997b551af98feff3555e02ad436c
Max Carey <maxcarey@protonmail.com>
2020-12-04 16:25:51 -0600

    Got web-dictaphone animation code working on html element here

--- static/js/app.js
+++ static/js/app.js
@@ -24,6 +24,14 @@ var googleHeardAudio = "";
 // Connect to the record button
 const recordButton = document.querySelector('#record-button');
 
+
+// Visaulizer set up
+const canvas = document.querySelector('.visualizer');
+let audioCtx;
+const canvasCtx = canvas.getContext("2d");
+
+
+
 // Set the default value as English in the drop down
 $('#ddl_lang_from').val('English (US)')
 
@@ -77,6 +85,7 @@ let recorder
       
       var chunks = [];
       recorder = new MediaRecorder(stream)
+      visualize(stream);
      
   
       // Convert blob to base 64 string when finished
@@ -292,4 +301,62 @@ var hearGoogleHeard = () => {
   var L2Audio = new Audio("data:audio/mp3;base64," + googleHeardAudio)
   L2Audio.play()
 
+}
+
+
+function visualize(stream) {
+  if(!audioCtx) {
+    audioCtx = new AudioContext();
+  }
+
+  const source = audioCtx.createMediaStreamSource(stream);
+
+  const analyser = audioCtx.createAnalyser();
+  analyser.fftSize = 2048;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  source.connect(analyser);
+  //analyser.connect(audioCtx.destination);
+
+  draw()
+
+  function draw() {
+    const WIDTH = canvas.width
+    const HEIGHT = canvas.height;
+
+    requestAnimationFrame(draw);
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+    canvasCtx.beginPath();
+
+    let sliceWidth = WIDTH * 1.0 / bufferLength;
+    let x = 0;
+
+
+    for(let i = 0; i < bufferLength; i++) {
+
+      let v = dataArray[i] / 128.0;
+      let y = v * HEIGHT/2;
+
+      if(i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+
+      x += sliceWidth;
+    }
+
+    canvasCtx.lineTo(canvas.width, canvas.height/2);
+    canvasCtx.stroke();
+
+  }
 }

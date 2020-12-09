cd17c937dc90043d31d6abc2feeef9fa43323cee
Max Carey <maxcarey@protonmail.com>
2020-12-09 14:30:11 -0600

    Disable settings button and add alerts to question mark button

--- static/js/app.js
+++ static/js/app.js
@@ -83,8 +83,9 @@ var initListeners = () => {
       hearAudio();
     });
     
-    
-    
+    $( "#question-mark" ).click(function() {
+      alert("We made this game so you can show off your language skills, and practice! Start out with easy words in languages you know. Try to say 'table', in Spanish. Then try languages you don't know. Try to say 'horse' in Thai. Language Horse works using Google translate's Speech-To-Text and Text-To-Speech features. Contact us! LanguageHorse@gmail.com, Credits: Project Leader: Graham Derry, Lead Developer: Max Carey (TOTAGO)")
+    });
     
     let recorder
     

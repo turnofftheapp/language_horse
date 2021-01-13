b0c3abc9c19769658488f9980b51428df9e32c16
Max Carey <maxcarey@protonmail.com>
2021-01-12 17:39:27 -0800

    Add canyousay route

--- app.py
+++ app.py
@@ -46,6 +46,23 @@ Talisman(app,
 	     content_security_policy=csp)
 
 
+@app.route('/canyousay/<string:L2TargetWord>/<string:translate_to_code>/', methods=['GET', 'POST'])
+def can_you_say(L2TargetWord, translate_to_code):
+
+	# Turn the list of tuples into a dictionary, reverse, dictionary
+	# And extract the friendly name for then language
+	# This is a bit of a hacky fix for the special endpoint
+	langDict = dict(langs)
+	langDictReversed = {y:x for x,y in langDict.items()}
+	friendlyName = langDictReversed[translate_to_code]
+
+	# This renders all of the appropriate information
+	return render_template('input.html',
+						   L2TargetWord=L2TargetWord,
+						   translate_to_code=translate_to_code,
+						   friendlyName=friendlyName)
+
+
 @app.route('/score/<string:translate_to_code>/<string:L2TargetWord>/<string:translate_from_code>', methods=['GET', 'POST'])
 def score(translate_to_code, L2TargetWord, translate_from_code):
 
